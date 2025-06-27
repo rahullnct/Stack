7e3e571e8800352d648c94bc61b2e35ec016118d
rahullnct <140547910+rahullnct@users.noreply.github.com>
2025-06-27 11:57:59 +0530

    conversion of Prefix to Postfix

---
+++ Stack7.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+#include<algorithm>
+using namespace std;
+string solve(string &s,int n)
+{
+    stack<string>st;
+    int i=n-1;
+    while(i>=0)
+    {
+        if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9'))
+        {
+            st.push(string(1,s[i]));
+        }
+        else{
+            string s1=st.top(); st.pop();
+            string s2=st.top(); st.pop();
+            string cons= s1+s2+s[i];
+            st.push(cons);
+        }
+        i--;
+    }
+    return st.top();
+}
+int main()
+{
+     string s="*-A/BC-/AKL";
+     int n=s.size();
+     string ans=solve(s,n);
+     cout<<ans;
+}
